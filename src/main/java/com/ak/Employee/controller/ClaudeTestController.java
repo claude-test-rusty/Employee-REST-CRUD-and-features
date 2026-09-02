6ca9f1225148207e517f3b7fd34ab3cf4b98e10a
Aniket Kaushik <aniket.kaushik@in2ittech.com>
2026-09-02 12:31:02 +0530

    git push

--- src/main/java/com/ak/Employee/controller/ClaudeTestController.java
+++ src/main/java/com/ak/Employee/controller/ClaudeTestController.java
@@ -4,7 +4,19 @@ import com.ak.Employee.dto.EmployeeDto;
 import com.ak.Employee.kafkaProducer.EmployeeEventProducer;
 import com.ak.Employee.response.ResponseHandler;
 import com.ak.Employee.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +46,12 @@ public class ClaudeTestController {
         List<String> dummyData = new ArrayList<String>();
         dummyData.add("Rusty");
         return ResponseHandler.responseBuilder("Here are the values of requested Employee", HttpStatus.OK, dummyData);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        int unused = 42;
+        return "test";
     }
 
 }
