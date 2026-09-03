8c9f0a37960356bc3be5479dca489031d55e43e9
Aniket Kaushik <aniket.kaushik@in2ittech.com>
2026-09-03 10:38:51 +0530

    Add idempotency test controller

---
+++ src/main/java/com/ak/Employee/controller/IdempotencyTestController.java
@@ -0,0 +1,14 @@
+package com.ak.Employee.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IdempotencyTestController {
+
+    @GetMapping("/idempotency-test")
+    public String test() {
+        String unusedVariable = "This intentionally creates a PMD finding";
+        return "Idempotency test";
+    }
+}
